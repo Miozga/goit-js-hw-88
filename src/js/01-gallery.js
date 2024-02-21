01a85ab8a6c2b1c48e45056d97696d721728313b
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-02-21 10:11:55 +0100

    m

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,5 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from "./gallery-items.js";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
 
-function renderGalleryItems(items) {
-  const galleryContainer = document.querySelector(".gallery");
-  const galleryMarkup = items
-    .map(
-      ({ preview, original, description }) =>
-        `<li class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" />
-            </a>
-        </li>`
-    )
-    .join("");
-
-  galleryContainer.innerHTML = galleryMarkup;
-}
-
-renderGalleryItems(galleryItems);
-
-new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
-});
+console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,1 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
 
-const iframe = document.getElementById("vimeo-player");
-const player = new Player(iframe);
-
-player.on(
-  "timeupdate",
-  throttle(function (data) {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
-  }, 1000)
-);
-
-const savedTime = localStorage.getItem("videoplayer-current-time");
-if (savedTime) {
-  player.setCurrentTime(savedTime).catch(function (error) {
-    console.error("Error occurred while setting video time: ", error);
-  });
-}
