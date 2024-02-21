e2d9edccc5f6658e48bd47fa61b2a309e8348c6e
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-02-21 11:31:37 +0100

    m

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+function renderGalleryItems(items) {
+  const galleryContainer = document.querySelector('.gallery');
+  const galleryMarkup = items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join('');
+
+  galleryContainer.innerHTML = galleryMarkup;
+}
+
+renderGalleryItems(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
